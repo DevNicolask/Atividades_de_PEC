344c43e9a428065b956c8d689c6ecf996e2fcdcf
Nícolas Kawan <dev.nicolask@gmail.com>
2026-08-17 23:49:35 -0300

    Add my solution to class 01 tasks

---
+++ dino.py
@@ -0,0 +1,32 @@
+print("Parabéns"+"!"*8)
+
+print("""
+Aqui vai um dinossauro!
+                         .       .
+                        / `.   .' \\
+                .---.  <    > <    >  .---.
+                |    \\  \\ - ~ ~ - /  /    |
+                 ~-..-~             ~-..-~
+             \\~~~\\.'                    `./~~~/
+   .-~~^-.    \\__/                        \\__/
+ .'  O    \\     /               /       \\  \\
+(_____,    `._.'               |         }  \\/~~~/
+ `----.          /       }     |        /    \\__/
+       `-.      |       /      |       /      `. ,~~|
+           ~-.__|      /_ - ~ ^|      /- _      `..-'   /: /:
+                |     /        |     /     ~-.     `-. _||_||_
+                |_____|        |_____|         ~ - . _ _ _ _ _>
+""")
+
+numero=30
+
+print("E aqui vai um bolo! "+"hu"+"m"*numero)
+print("")
+print("."*numero)
+print("i"*numero)
+print("#"*numero)
+print("="*numero)
+print("#"*numero)
+print("="*numero)
+print("#"*numero)
+print("")
---
+++ dog.py
@@ -0,0 +1,5 @@
+print("""
+Uma imagem de um cachorro...
+            o____
+             ||||
+""")
