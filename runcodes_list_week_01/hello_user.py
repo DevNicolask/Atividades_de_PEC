8277309c04a36443866e18dba5b18eb3133e83f3
NICOLAS KAWAN DOS SANTOS CARDOSO <nicolas.cardoso@equatorialservicos.com.br>
2026-08-24 09:25:53 -0300

    Removed a latest version to sourse

--- runcodes_list_week_01/hello_user.py
+++
@@ -1,4 +0,0 @@
-# Entrada de dados
-nome = input('Digite o seu nome pelo teclado: ').strip()
-# Saída de dados
-print(f"Seja bem-vindo(a), {nome}!")
--- runcodes_list_week_01/sum_int.py
+++
@@ -1,9 +0,0 @@
-# Entrada de dados
-a = float(input("Digite o primeiro número: "))
-b = float(input("Digite o segundo número: "))
-
-# Processamento
-soma = a + b
-
-# Saída de dados
-print(f"A soma entre {a:.2f} e {b:.2f} é {soma:.2f}")
